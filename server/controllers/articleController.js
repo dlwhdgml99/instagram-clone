6d83dc174c0ad09fa8fef9423ee4955aa8946f1c
dlwhdgml99 <jjonghee99@naver.com>
2023-06-09 21:52:19 +0900

    6/9

--- server/controllers/articleController.js
+++ server/controllers/articleController.js
@@ -4,10 +4,45 @@ const Article = require('../models/Article');
 const Favorite = require('../models/Favorite');
 // 파일 처리 라이브러리
 const fileHandler = require('../utils/fileHandler');
+const { json } = require('express');
 
 
 // 피드 가져오기 로직
-exports.feed = async (req, res, next) => {};
+exports.feed = async (req, res, next) => {
+	try {
+		// Follow 컬렉션의 도큐먼트 중 팔로워가 로그인 유저인 도큐먼트 
+		const follows = await Follow.find({ follower: req.user._id });
+		// follows에서 following 값만 추출하여 리스트를 생성한다
+		// 로그인 유저가 팔로우하는 유저들의 id 리스트
+		const followings = follows.map(follow => follow.following);
+
+		const where = { author: [...followings, req.user._id] } // 검색 조건
+		const limit = req.query.limit || 5; // 한번 클라이언트에 전송할 때 보낼 도큐먼트의 수
+		const skip = req.query.skip || 0; // 건너 뛸 도큐먼트의 수
+
+		const articleCount = await Article.count(where); // 게시물 개수
+		// 
+		const articles = await Article
+			.find(where)
+			.populate({ // 컬렉션 조인
+				path: 'author',
+				select: 'username avatar' // 필드 선택
+			})
+			.populate({
+				path: 'isFavorite'
+			})
+			.populate({
+				path: 'commentCount'
+			})
+			.sort({ created: 'desc' })
+			.skip(skip)
+			.limit(limit)
+		res.json({ articles, articleCount });	
+
+	} catch (error) {
+		next(error)
+	}
+};
 
 // 게시물 가져오기 로직
 exports.find = async (req, res, next) => {
@@ -68,10 +103,63 @@ exports.findOne = async (req, res, next) => {
 };
 
 // 게시물 생성하기 로직
-exports.create = [];
+exports.create = [
+	fileHandler("articles").array("photos"), // 파일 처리
+	async (req, res, next) => {
+		try {
+			const files = req.files;
+
+			if (files.length < 1) { // 파일이 없을 경우
+				const err = new Error('File is required');
+				err.status = 400;
+				throw err;
+			}
+
+			const photos = files.map(file => file.filename); // 파일 이름으로 이루어진 배열을 생성한다
+
+			const article = new Article({
+				photos, // 파일 이름 리스트
+				description: req.body.description,
+				author: req.user._id // 로그인 유저
+			});
+
+			await article.save(); // 저장
+
+			res.json({ article }); 
+
+		} catch (error) {
+			next(error)
+		}
+	}
+];
 
 // 게시물 삭제
-exports.delete = async (req, res, next) => {};
+exports.delete = async (req, res, next) => {
+	try {
+		// id parameter로 삭제할 게시물을 검색한다
+		const article = await Article.findById(req.params.id);
+
+		if (!article) { // 게시물이 존재하지 않을 경우
+			const err = new Error("Article not found")
+			err.status = 404; // 리소스 없음
+			throw err;
+		}
+
+		// 본인 게시물이 아닌 경우
+		if (req.user._id.toString() !== article.author.toString()) {
+			const err = new Error("Author is not correct")
+			err.status = 400;
+			throw err;
+		}
+
+		await article.deleteOne();
+
+		res.json({ article }); // 삭제한 게시물을 전송한다
+
+	} catch (error) {
+		next(error)
+	}
+};
 
 // 좋아요
 exports.favorite = async (req, res, next) => {
--- server/controllers/profileController.js
+++ server/controllers/profileController.js
@@ -1,16 +1,178 @@
 const User = require('../models/User');
 const Follow = require('../models/Follow');
 const Article = require('../models/Article');
+const { favorite } = require('./articleController');
 
 
 // 프로필 리스트 가져오기
-exports.find = async (req, res, next) => {};
+exports.find = async (req, res, next) => {
+	try {
+		const where = {};
+		const limit = req.query.limit || 10;
+		const skip = req.query.skip || 0;
+
+		// 특정 유저가 팔로우하는 프로필 리스트
+		if ('following' in req.query) {
+			const user = await User.findOne({ username: req.query.following });
+			const follows = await Follow
+				.find({ follower: user._id })
+
+			where._id = follows.map(follow => follow.following);
+		}
+
+		//특정 게시물을 좋아하는 프로필 리스트
+		if('favorite' in req.query) {
+			const favorites = await Favorite.find({ article: req.query.favorite })
+
+			where._id = favorites.map(favorite => favorite.user);
+		}
+
+		
+		if ('followers' in req.query) {
+			const user = await User.findOne({ username: req.query.followers });
+			const follows = await Follow
+				.find({ following: user._id })
+			
+			where._id = follows.map(follow => follow.follower);
+		}
+
+		// 특정 글자를 유저이름에 포함한 프로필 리스트
+		if ('username' in req.query) {
+			// 정규식 사용
+			where.username = new RegExp(req.query.username, 'i');
+		}
+
+		// 프로필 검색
+		const profileCount = await User.count(where); // 프로필 개수
+
+		const profiles = await User
+			.find(where, 'username fullname avatar bio')
+			.populate({
+				path: 'isFollowing', // Follow 컬렉션과 조인
+				match: { follower: req.user._id } // 로그인 유저가 팔로우 중인 프로필인지 확인
+			})
+			.limit(limit)
+			.skip(skip)
+
+		res.json({ profiles, profileCount });
+
+	} catch (error) {
+		next(error)
+	}
+};
 
 // 프로필 한개 가져오기
-exports.findOne = async (req, res, next) => {};
+exports.findOne = async (req, res, next) => {
+	try {
+		const _profile = await User
+			.findOne({ username: req.params.username }, 'username fullName avatar bio')
+			.populate({
+				path: 'isFollowing', // Follow 컬렉션과 조인
+				match: { follower: req.user._id } // 로그인 유저가 팔로우 중인 프로필인지 확인 가능
+			})
+
+		if (!profile) { // 프로필이 존재하지 않을 경우
+			const err = new Error("Profile not found");
+			err.status = 404;
+			throw err;
+		}
+
+		const {
+			username,
+			fullName,
+			avatar,
+			bio,
+			isFollowing
+		} = _profile;
+
+		// 프로필 유저의 팔로잉 수
+		const followingCount = await Follow.count({ follower: _profile._id })
+		// 프로필 유저의 팔로워 수
+		const followerCount = await Follow.count({ following: _profile._id })
+		// 프로필 유저의 게시물 수
+		const articleCount = await Article.count({ author: _profile._id })
+
+		const profile = {
+			username,
+			fullName,
+			avatar,
+			bio,
+			isFollowing,
+			followingCount,
+			followerCount,
+			articleCount
+		}
+
+		res.json({ profile });
+
+	} catch (error) {
+		next(error)
+	}
+};
 
 // 팔로우 처리
-exports.follow = async (req, res, next) => {};
+exports.follow = async (req, res, next) => {
+	try {
+		// 본인을 팔로우 요청한 경우
+		if (req.user.username === req.params.username) {
+			const err = new Error('Cannot Follow yourself')
+			err.status = 400;
+			throw err;
+		}
+		// 팔로우 할 프로필을 검색한다
+		const profile = await User
+			.findOne({ username: req.params.username }, 'username fullName avatar bio');
+		
+			if (!profile) { // 프로필이 존재하지 않을 경우
+				const err = new Error('Profile not found')
+				err.status = 404;
+				throw err;
+			}
+		
+		// 이미 팔로우 중인 프로필일 경우
+		const _follow = await Follow
+			.findOne({ follower: req.user._id, following: profile._id })
+		
+		if (!_follow) { // 새 팔로우인 경우
+			const follow = new Follow({
+				follower: req.user._id,
+				following: profile._id
+			})
+
+			await follow.save();
+		}
+
+		res.json({ profile }); // 방금 팔로우한 프로필을 전송한다
+
+	} catch (error) {
+		next(error)
+	}
+};
 
 // 팔로우 취소 처리
-exports.unfollow = async (req, res, next) => {};
+exports.unfollow = async (req, res, next) => {
+	try {
+		// 팔로우를 취소할 프로필을 검색한다
+		const username = req.params.username;
+		const profile = await User.findOne({ username }, 'username fullName avatar bio');
+
+		if (!profile) { // 프로필이 존재하지 않는 경우
+			const err = new Error('Profile not found')
+			err.status = 404;
+			throw err;
+		}
+
+		// 현재 팔로우 중인 프로필인지 확인한다
+		const follow = await Follow
+			.findOne({ follower: req.user._id, following: profile._id });
+		
+		if (follow) { // 팔로우중이면 팔로우를 취소한다
+			await follow.deleteOne();
+		}
+
+		res.json({ profile }); // 팔로우를 취소한 프로필을 전송한다
+
+	} catch (error) {
+		next(error)
+	}
+};
