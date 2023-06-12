646aa1f2122a9dfbffa65503928d40b9a3cc9017
dlwhdgml99 <jjonghee99@naver.com>
2023-06-12 21:42:22 +0900

    6/12

--- client/src/components/AuthRequired.js
+++ client/src/components/AuthRequired.js
@@ -1,13 +1,15 @@
 // 인증 상태 관리(검사)
-import { Children, useContext, useEffect, useState } from "react";
+import { useContext} from "react";
 import { Navigate } from "react-router-dom";
 import AuthContext from "./AuthContext";
 
 
-export default function AuthRequired() {
+export default function AuthRequired({ children }) {
+
+	// user 객체에 접근할 수 있다
 	const { user } = useContext(AuthContext);
 
-	if (!user) {
+	if (!user) { // 인증 실패 : 로그인 페이지로 이동한다
 		return <Navigate to="/accounts/login" replace={true} />
 	}
 
