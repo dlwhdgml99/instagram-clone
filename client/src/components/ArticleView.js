0b3af04e4cbccc6415e95d228b0f1e8dd53104ec
dlwhdgml99 <jjonghee99@naver.com>
2023-06-21 21:39:49 +0900

    6/21

--- client/src/components/ArticleView.js
+++ client/src/components/ArticleView.js
@@ -25,13 +25,61 @@ export default function ArticleView() {
 	console.log(article);
 
 	// 좋아요 처리
-	async function handleFavorite(id) {}
+	async function handleFavorite(id) {
+		try {
+			// 서버에 좋아요 요청
+			await favorite(id)
+	  
+			// article 업데이트
+			const updatedArticle = {
+			  ...article,
+			  isFavorite: true,
+			  favoriteCount: article.favoriteCount + 1
+			}
+	  
+			setArticle(updatedArticle);
+	  
+		  } catch (error) {
+			alert(error)
+		  }
+		}
+	
 
 	// 좋아요 취소 처리
-	async function handleUnFavorite(id) {}
+	async function handleUnfavorite(id) {
+		try {
+			// 서버에 좋아요 취소처리 요청
+			await unfavorite(id);
+	  
+			// article 업데이트
+			const updatedArticle = {
+			  ...article,
+			  isFavorite: false,
+			  favoriteCount: article.favoriteCount - 1
+			}
+	  
+			setArticle(updatedArticle);
+		  
+		  } catch (error) {
+			alert(error)
+		  }
+	}
+
+	console.log(handleUnfavorite);
 
 	// 게시물 삭제 처리
-	async function handleDelete(id) {}
+	async function handleDelete(id) {
+		try {
+			// 서버에 게시물 삭제 요청
+			await deleteArticle(id);
+			
+			// 피드로 이동
+			navigate('/', { replace: true });
+		  
+		  } catch (error) {
+			alert(error)
+		  }
+	}
 
 	if (!article) {
 		return <Spinner />
@@ -41,7 +89,7 @@ export default function ArticleView() {
 		<ArticleTemplate 
 			article={article}
 			handleFavorite={handleFavorite}
-			handleUnFavorite={handleUnFavorite}
+			handleUnfavorite={handleUnfavorite}
 			handleDelete={handleDelete}
 		/>
 	)
--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,1 +1,126 @@
-export default function Comments() {}
+import { useContext, useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getComments, createComment, deleteComment } from "../utils/requests";
+import Spinner from "./Spinner";
+
+// 메인 컴포넌트
+export default function Comments() {
+	// 게시물의 아이디
+	const { id } = useParams();
+	const [error, setError] = useState(null);
+	const [isLoaded, setIsLoaded] = useState(false);
+	// 댓글 목록
+	const [comments, setComments] = useState([]);
+	// 댓글 개수
+	const [commentCount, setCommentCount] = useState(0);
+
+	// 서버에 댓글 가져오기 요청
+	useEffect(() => {
+		getComments(id)
+		.then(data => {
+			setComments([...comments, ...data.comments]);
+			setCommentCount(data.commentCount);
+		})
+		.catch(error => {
+			setError(error);
+		})
+		.finally(() => setIsLoaded(true));
+	}, [])
+
+	console.log(comments)
+	console.log(commentCount)
+
+	// 댓글 추가
+	async function handleAddComment(content) {
+		try {
+			// 서버에 댓글 생성 요청
+			const data = await createComment(id, content);
+
+			// 댓글 개수를 1 증가시킨다
+			setCommentCount(commentCount + 1);
+			
+			// comments 업데이트
+			const updatedComments = [data.comment, ...comments];
+			setComments(updatedComments);
+
+		} catch (error) {
+			alert(error)
+		}
+	}
+
+	// 댓글 삭제
+	async function handleDelete(id) {}
+
+	// 댓글 리스트
+	const commentList = comments.map(comment => (
+		// 각각의 댓글
+		<Comment 
+			key={comment.id}
+			comment={comment}
+			handleDelete={handleDelete}
+		/>
+	))
+
+	return (
+		<div className="px-4">
+			<h3 className="text-lg font-semibold my-4">댓글보기</h3>
+			<Form 
+				handleAddComment={handleAddComment}
+			/>
+
+			{commentCount > 0 ? (
+				<ul>
+					{commentList}
+				</ul>
+			) : (
+				<p className="text-center">첫번째로 댓글을 달아보세요</p>
+			)}
+
+			{!isLoaded && <Spinner />}
+			{error && <p className="text-red-500">{error.message}</p>}
+		</div>
+	)
+}
+
+// 폼
+function Form({ handleAddComment }) {
+	// 댓글 내용
+	const [content, setContent] = useState("");
+
+	// 폼 제출 처리
+	async function handleSubmit(e) {
+		try {
+			e.preventDefault();
+
+			await handleAddComment(content);
+
+			setContent(""); // 댓글 입력칸을 비운다
+
+		} catch (error) {
+			alert(error)
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className="mb-4">
+			<textarea 
+			rows="2"
+			className="border w-full px-2 py-1 rounded resize-none"
+			value={content}
+			onChange={({ target }) => setContent(target.value)}
+			/>
+			<button
+				type="submit"
+				className="bg-blue-500 text-white text-sm font-semibold px-4 py-2 rounded-lg disabled:opacity-[0.2]"
+				disabled={!content.trim()}
+			>
+				댓글 달기
+			</button>
+		</form>
+	)
+
+
+}
+
+// 댓글 아이템
+function Comment() {}
